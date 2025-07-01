59f1eec414f1893a42bc2f2e9b42b3d0dabba68e
ashurovdev <ashurovdev@gmail.com>
2025-07-01 19:03:16 +0500

    Python

--- lesson-16/homework/task2-4.py
+++ lesson-16/homework/task2-4.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data_csv = pd.read_csv('../data/movie.csv')
-movie2h = data_csv[data_csv['duration'] >= 200]
+movie2h = data_csv[data_csv['duration'] >= 120]
 des_facebook_likes = data_csv.sort_values(by='director_facebook_likes', ascending=False)
 print(movie2h)
 print(des_facebook_likes)
